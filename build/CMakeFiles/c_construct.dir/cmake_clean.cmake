18046d3f13c51a4cb0e82d9a9a8977f20b15fedf
permalik <permalik@protonmail.com>
2025-03-02 17:13:32 -0600

    feat: init

---
+++ build/CMakeFiles/c_construct.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c_construct.dir/src/main.c.o"
+  "CMakeFiles/c_construct.dir/src/main.c.o.d"
+  "c_construct"
+  "c_construct.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c_construct.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    const char SOURCEDIRECTORY[] = "data";
+    const char DESTDIRECTORY[] = "dest";
+
+    // TODO: assert inputFiles is not empty
+    // TODO: remove stale dirs
+    // TODO: make new dirs
+    // TODO: make files if source
+
+    printf("%s%s", SOURCEDIRECTORY, "\n___\n");
+    printf("%s%s", DESTDIRECTORY, "\n___\n");
+    return 0;
+}
