62c33dcbd4a3ea9f4684cf3bf3d547643fa1c123
permalik <permalik@protonmail.com>
2025-03-03 19:13:15 -0600

    feat: stat in initDirectory

--- src/main.c
+++ src/main.c
@@ -1,13 +1,37 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/syslimits.h>
+#include <unistd.h>
 
 struct Files {
     char *file;
 };
 
+int init_directory(const char **dir_path/*,char *filenames[]*/) {
+    // TODO: remove stale dirs
+    struct stat s;
+    if (stat(*dir_path, &s) == 0) {
+        if (S_ISDIR(s.st_mode)) {
+            printf("Found directory: %s", *dir_path);
+            return 1;
+        } else {
+            printf("Path exists but is not directory.\n");
+        }
+    } else {
+        if (errno == ENOENT) {
+            printf("Directory does not exist\n");
+        } else {
+            perror("Failed to stat.\n");
+        }
+    }
+    // TODO: make new dirs
+    // TODO: make files if source
+    return 0;
+}
+
 int main() {
-    const char *SOURCEDIRECTORY = "data";
-    const char *DESTDIRECTORY = "dest";
     const char *filenames[] = {
         "one.txt", "two.txt", "three.txt", "four.txt", "five.txt",
         "six.txt", "seven.txt", "eight.txt", "nine.txt", "ten.txt",
@@ -28,10 +52,10 @@ int main() {
         printf("%s\n", filenames[i]);
     }
 
-    // TODO: assert inputFiles is not empty
-    // TODO: remove stale dirs
-    // TODO: make new dirs
-    // TODO: make files if source
+    const char *SOURCEDIRECTORY = "/Users/tymalik/Docs/Git/construct/c_construct/src/data";
+    const char *DESTDIRECTORY = "dest";
+    int is_source_initialized = init_directory(&SOURCEDIRECTORY);
+    assert((is_source_initialized == 1) && "Source must be initialized.");
 
     printf("%s%s", SOURCEDIRECTORY, "\n___\n");
     printf("%s%s", DESTDIRECTORY, "\n___\n");
