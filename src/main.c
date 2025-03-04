b0f98265743064b73666d427c0c3d620ee14853f
permalik <permalik@protonmail.com>
2025-03-03 21:35:53 -0600

    feat: print files from populate_source

--- src/main.c
+++ src/main.c
@@ -5,19 +5,6 @@
 #include <sys/syslimits.h>
 #include <unistd.h>
 
-const char *filenames[] = {
-    "one.txt", "two.txt", "three.txt", "four.txt", "five.txt",
-    "six.txt", "seven.txt", "eight.txt", "nine.txt", "ten.txt",
-    "eleven.txt", "twelve.txt", "thirteen.txt", "fourteen.txt", "fifteen.txt",
-    "sixteen.txt", "seventeen.txt", "eighteen.txt", "nineteen.txt", "twenty.txt",
-    "twenty_one.txt", "twenty_two.txt", "twenty_three.txt", "twenty_four.txt", "twenty_five.txt",
-    "twenty_six.txt", "twenty_seven.txt", "twenty_eight.txt", "twenty_nine.txt", "thirty.txt",
-    "thirty_one.txt", "thirty_two.txt", "thirty_three.txt", "thirty_four.txt", "thirty_five.txt",
-    "thirty_six.txt", "thirty_seven.txt", "thirty_eight.txt", "thirty_nine.txt", "forty.txt",
-    "forty_one.txt", "forty_two.txt", "forty_three.txt", "forty_four.txt", "forty_five.txt",
-    "forty_six.txt", "forty_seven.txt", "forty_eight.txt", "forty_nine.txt", "fifty.txt"
-};
-
 struct Files {
     char *file;
 };
@@ -43,16 +30,36 @@ int init_directory(const char **dir_path) {
     return 1;
 }
 
-int main() {
-    int filenames_length = sizeof(filenames) / sizeof(filenames[0]);
-    assert(filenames_length > 0 && "filenames cannot be empty");
+int populate_source(const char **dir_path, const char *source_file_names[], int *filenames_length) {
+    for (int i = 0; i < *filenames_length; i++) {
+        printf("%s/%s\n", *dir_path, source_file_names[i]);
+    }
+    return 1;
+}
 
+int main() {
     const char *SOURCEDIRECTORY = "/Users/tymalik/Docs/Git/construct/c_construct/src/data";
     const char *DESTDIRECTORY = "/Users/tymalik/Docs/Git/construct/c_construct/src/dest";
     int is_source_initialized = init_directory(&SOURCEDIRECTORY);
     assert((is_source_initialized == 1) && "Source must be initialized.");
     int is_dest_initialized = init_directory(&DESTDIRECTORY);
     assert((is_dest_initialized == 1) && "Dest must be initialized.");
+
+    const char *filenames[] = {
+        "one.txt", "two.txt", "three.txt", "four.txt", "five.txt",
+        "six.txt", "seven.txt", "eight.txt", "nine.txt", "ten.txt",
+        "eleven.txt", "twelve.txt", "thirteen.txt", "fourteen.txt", "fifteen.txt",
+        "sixteen.txt", "seventeen.txt", "eighteen.txt", "nineteen.txt", "twenty.txt",
+        "twenty_one.txt", "twenty_two.txt", "twenty_three.txt", "twenty_four.txt", "twenty_five.txt",
+        "twenty_six.txt", "twenty_seven.txt", "twenty_eight.txt", "twenty_nine.txt", "thirty.txt",
+        "thirty_one.txt", "thirty_two.txt", "thirty_three.txt", "thirty_four.txt", "thirty_five.txt",
+        "thirty_six.txt", "thirty_seven.txt", "thirty_eight.txt", "thirty_nine.txt", "forty.txt",
+        "forty_one.txt", "forty_two.txt", "forty_three.txt", "forty_four.txt", "forty_five.txt",
+        "forty_six.txt", "forty_seven.txt", "forty_eight.txt", "forty_nine.txt", "fifty.txt"
+    };
+    int filenames_length = sizeof(filenames) / sizeof(filenames[0]);
+    assert(filenames_length > 0 && "filenames cannot be empty");
+    populate_source(&SOURCEDIRECTORY, filenames, &filenames_length);
 
     return 0;
 }
