303075c921a7c9ff0aaaebf7c6a97a34f9d46683
permalik <permalik@protonmail.com>
2025-03-03 23:38:56 -0600

    feat: write to files

--- src/main.c
+++ src/main.c
@@ -33,19 +33,44 @@ int init_directory(const char** dir_path) {
 }
 
 int populate_source(const char** dir_path, const char* source_file_names[], int* file_names_length) {
+    int is_ok = EXIT_SUCCESS;
     for (int i = 0; i < *file_names_length; i++) {
         size_t path_length = strlen(*dir_path) + strlen(source_file_names[i]);
 
         char* file_path = malloc(path_length);
         if (file_path == NULL) {
-            fprintf(stderr, "Memory allocation failure in populate_source.\n");
-            return 1;
+            fprintf(stderr, "Error: Memory allocation failed for %s\n", source_file_names[i]);
+            is_ok = EXIT_FAILURE;
+            continue;
         }
 
         snprintf(file_path, path_length, "%s/%s", *dir_path, source_file_names[i]);
-        printf("%s\n", file_path);
+        FILE* fp = fopen(file_path, "w+");
+        if (!fp) {
+            perror("File opening failed.\n");
+            is_ok = EXIT_FAILURE;
+            continue;
+        }
+        fputs("test_string\n", fp);
+        rewind(fp);
 
+        int c;
+        while ((c = fgetc(fp)) != EOF) {
+            putchar(c);
+        }
+
+        if (ferror(fp)) {
+            puts("I/O error when reading.");
+        } else if (feof(fp)) {
+            puts("End of file successfully reached.");
+        }
+
+        fclose(fp);
         free(file_path);
+    }
+
+    if (is_ok == EXIT_FAILURE) {
+        fprintf(stderr, "Memory allocations failed.\n");
     }
     return 0;
 }
