9b58cb0d917b8af97a41c6891d1fc17bb26a4ade
permalik <permalik@protonmail.com>
2025-03-28 00:04:57 -0500

    feat: impl devshell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "auto";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = false;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.alejandra
+            pkgs.gcc
+            pkgs.gnumake
+            pkgs.pkg-config
+            pkgs.llvmPackages_19.clang-tools
+            # pkgs.gdb
+            # pkgs.valgrind
+          ];
+
+          shellHook = ''
+            # Custom Prompt
+            export PS1="\n\[\e[1;32m\][devshell](c) \w\n❯ \[\e[0m\]"
+          '';
+        };
+      }
+    );
+}
